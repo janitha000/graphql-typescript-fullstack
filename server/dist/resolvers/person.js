0ee42b0518b307abca03c25db32a42ce9c1b8800
Janitha Tennakoon <janitha000@gmail.com>
2021-04-21 15:33:07 +0800

    Add CRUD operations

--- server/dist/resolvers/person.js
+++ server/dist/resolvers/person.js
@@ -18,13 +18,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.PersonResolver = void 0;
 const type_graphql_1 = require("type-graphql");
 const Person_1 = __importDefault(require("../models/Person"));
+let personList = [
+    new Person_1.default(1, "Janitha", "Tennakoon", 31),
+    new Person_1.default(2, "Vindya", "Hettige", 30),
+];
 class PersonResolver {
-    posts({ auth }) {
+    persons({ auth }) {
         console.log(auth);
-        return [
-            new Person_1.default(1, "Janitha", "Tennakoon", 31),
-            new Person_1.default(2, "Vindya", "Hettige", 30),
-        ];
+        return personList;
+    }
+    person(id) {
+        return personList.find(x => x.id === parseInt(id));
+    }
+    createPerson(id, firstName, lastName, age) {
+        let newPerson = new Person_1.default(parseInt(id), firstName, lastName, age);
+        personList.push(newPerson);
+        return newPerson;
     }
 }
 __decorate([
@@ -32,7 +41,24 @@ __decorate([
     __param(0, type_graphql_1.Ctx()),
     __metadata("design:type", Function),
     __metadata("design:paramtypes", [Object]),
-    __metadata("design:returntype", void 0)
-], PersonResolver.prototype, "posts", null);
+    __metadata("design:returntype", Array)
+], PersonResolver.prototype, "persons", null);
+__decorate([
+    type_graphql_1.Query(() => Person_1.default, { nullable: true }),
+    __param(0, type_graphql_1.Arg('id', () => type_graphql_1.ID)),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String]),
+    __metadata("design:returntype", Object)
+], PersonResolver.prototype, "person", null);
+__decorate([
+    type_graphql_1.Mutation(() => Person_1.default),
+    __param(0, type_graphql_1.Arg('id')),
+    __param(1, type_graphql_1.Arg('firstName')),
+    __param(2, type_graphql_1.Arg('lastName')),
+    __param(3, type_graphql_1.Arg('age')),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String, String, String, Number]),
+    __metadata("design:returntype", Person_1.default)
+], PersonResolver.prototype, "createPerson", null);
 exports.PersonResolver = PersonResolver;
 //# sourceMappingURL=person.js.map
--- server/src/resolvers/person.ts
+++ server/src/resolvers/person.ts
@@ -1,15 +1,34 @@
-import { Ctx, Query } from 'type-graphql';
+import { Arg, Ctx, ID, Mutation, Query } from 'type-graphql';
 import Person from '../models/Person'
 import { MyContext } from '../types'
 
+let personList = [
+    new Person(1, "Janitha", "Tennakoon", 31),
+    new Person(2, "Vindya", "Hettige", 30),
+]
 export class PersonResolver {
     @Query(() => [Person])
-    posts(@Ctx() { auth }: MyContext) {
+    persons(@Ctx() { auth }: MyContext): Person[] {
         console.log(auth)
-        return [
-            new Person(1, "Janitha", "Tennakoon", 31),
-            new Person(2, "Vindya", "Hettige", 30),
-        ]
+        return personList;
+    }
+
+    @Query(() => Person, { nullable: true })
+    person(@Arg('id', () => ID) id: string): Person | undefined {
+        return personList.find(x => x.id === parseInt(id));
+    }
+
+    @Mutation(() => Person)
+    createPerson(
+        @Arg('id') id: string,
+        @Arg('firstName') firstName: string,
+        @Arg('lastName') lastName: string,
+        @Arg('age') age: number,
+    ): Person {
+        let newPerson = new Person(parseInt(id), firstName, lastName, age);
+        personList.push(newPerson)
+
+        return newPerson
     }
 
 }
